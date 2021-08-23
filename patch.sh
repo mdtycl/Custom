1c86e010061cc43de8e151c111410101bac4273c
mdtycl <70188230+mdtycl@users.noreply.github.com>
2021-08-23 18:57:34 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -15,7 +15,7 @@ TIME() {
 	z) export Color="\e[35m";;
 	l) export Color="\e[36m";;
       esac
-	[[ $# -lt 2 ]] && echo -e "\e[36m\e[0m ${1}" || {
+	[[ $# -lt 2 ]] && echo "\e[36m\e[0m ${1}" || {
 		echo -e "\e[36m\e[0m ${Color}${2}\e[0m"
 	 }
       }
